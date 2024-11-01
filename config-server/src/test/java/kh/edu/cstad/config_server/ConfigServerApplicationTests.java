3128c4c807b97dc1a0da43c01d689e2ce1c9b2bf
soknem <povsoknem@gmail.com>
2024-11-01 15:57:01 +0700

    config server and recource in local using profile native

---
+++ config-server/src/test/java/kh/edu/cstad/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.edu.cstad.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
