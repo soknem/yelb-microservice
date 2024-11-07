cb654642cab0a8e4c9ddd26f709065937784b100
soknem <povsoknem@gmail.com>
2024-11-07 17:03:43 +0700

    add security and use external config

--- identity/src/main/java/kh/edu/cstad/identity/config/CoreConfig.java
+++ identity/src/main/java/kh/edu/cstad/identity/config/CoreConfig.java
@@ -4,15 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//public class CoreConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**") // Allow CORS on all paths
-//                .allowedOrigins("http://localhost:3000") // Allow only requests from this origin
-//                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow specific HTTP methods
-//                .allowedHeaders("*") // Allow all headers
-//                .allowCredentials(true);
-//    }
-//}
+@Configuration
+public class CoreConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow CORS on all paths
+                .allowedOrigins("http://localhost:3000") // Allow only requests from this origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow specific HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true);
+    }
+}
